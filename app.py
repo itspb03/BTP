196893cc00085a3fb13c59d14a25e21198a7f89d
itspb03 <173836660+itspb03@users.noreply.github.com>
2025-01-25 00:16:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 # Load Excel sheets
 mpbx_data = pd.read_excel(r"https://github.com/itspb03/BTP/blob/main/data/MPBX%20AS%20ON%2013.11.2024.xlsx", sheet_name=None)
-pressure_data = pd.read_excel(r"data/PRESSURE CELL AS ON 13.11.2024.xlsx", sheet_name=None)
+pressure_data = pd.read_excel(r"https://github.com/itspb03/BTP/blob/main/data/PRESSURE%20CELL%20AS%20ON%2013.11.2024.xlsx", sheet_name=None)
 
 pd.set_option('future.no_silent_downcasting', True)
 
